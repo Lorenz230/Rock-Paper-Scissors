e58a7ef3858f3c5cbb7b6904553f28d52e575190
Lorenz230 <lorenzcamento0@gmail.com>
2023-07-24 17:46:42 +0100

    added untracked files.

--- script.js
+++ script.js
@@ -1,3 +1,88 @@
-getComputerChoice(){
+function getComputerChoice(){
     let x = Math.floor(Math.random() * 3);
+    if (x == 0) {
+        return "rock";
+    }
+    if (x == 1) {
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
 }
+
+function getPlayerChoice(){
+    let choice = prompt("Pick rock, paper or scissors");
+    choice = choice.toLowerCase();
+    console.log(choice);
+    return choice;
+    
+}
+
+computerChoice = getComputerChoice();
+playerChoice = getPlayerChoice();
+
+function playRound(playerChoice, computerChoice){
+    let text;
+    if (playerChoice == "rock"){
+        if (computerChoice == "paper"){
+            text = `You lost! ${playerChoice} loses to ${computerChoice}`;
+            console.log(text);
+        }
+
+        if (computerChoice == "rock"){
+            text = `Draw ${playerChoice} draws ${computerChoice}`;
+            console.log(text);
+        }
+
+        if (computerChoice == "scissors"){
+            text = `You Win! ${playerChoice} beats ${computerChoice}`;
+            console.log(text);
+        }
+
+    }
+
+    if (playerChoice == "paper"){
+        if (computerChoice == "scissors"){
+            text = `You lost! ${playerChoice} loses to ${computerChoice}`;
+            console.log(text);
+        }
+
+        if (computerChoice == "paper"){
+            text = `Draw ${playerChoice} draws ${computerChoice}`;
+            console.log(text);
+        }
+
+        if (computerChoice == "rock"){
+            text = `You Win! ${playerChoice} beats ${computerChoice}`;
+            console.log(text);
+        }
+
+    }
+
+    if (playerChoice == "scissors"){
+        if (computerChoice == "rock"){
+            text = `You lost! ${playerChoice} loses to ${computerChoice}`;
+            console.log(text);
+        }
+
+        if (computerChoice == "scissors"){
+            text = `Draw ${playerChoice} draws ${computerChoice}`;
+            console.log(text);
+        }
+
+        if (computerChoice == "paper"){
+            text = `You Win! ${playerChoice} beats ${computerChoice}`;
+            console.log(text);
+        }
+
+    }
+
+
+    
+    
+}
+
+playRound(playerChoice, computerChoice);
+
+
